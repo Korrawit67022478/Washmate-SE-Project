425609285b4746affe3c01627d5e34ebfd8eb0d9
Korrawit67022478 <67022478@up.ac.th>
2025-03-01 17:46:21 +0700

    Final 1.0

---
+++ Washmate/JS/handleLogin.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const authButton = document.getElementById("authButton");
+    const profileImg = document.getElementById("userImage")
+    const authText = document.getElementById("authText")
+
+
+function updateAuthButton() {
+    const isLoggedIn = localStorage.getItem("loggedIn");
+    
+    if (isLoggedIn) {
+        profileImg.style.display = "inline"
+        authText.textContent = "Noname"
+        authButton.style.backgroundColor = "transparent"
+        authButton.style.border = "none"
+        authButton.style.color = "white"
+
+        authButton.onclick = function () {
+            window.location.href = "profile.html";
+        };
+        authButton.onmouseover = function() {
+            authButton.style.backgroundColor = "#0056b3";
+            authButton.style.borderRadius = "5px";
+            authButton.style.cursor = "pointer"
+        };
+
+        authButton.onmouseout = function() {
+            authButton.style.backgroundColor = "transparent";
+        };
+
+    } else {
+        authButton.textContent = "Login";
+        authButton.onclick = function () {
+            window.location.href = "../login.html";
+        };
+    }
+}
+
+
+updateAuthButton();
+});
